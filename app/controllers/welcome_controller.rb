372a037c82e79d30a2e4437a99c3579b138b5660
maryhipp <maryhipp@gmail.com>
2014-08-23 13:38:39 -0400

    added admin to group seeds

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,6 @@ class WelcomeController < ApplicationController
 
   # skip_before_action :authenticate_user!
   def index
-    binding.pry
     @group = Group.find(current_user.group_id)
     @admin = User.find(@group.user_id)
   end
