e901bb0979822d2dac5256cfe0f9830f64863785
maryhipp <maryhipp@gmail.com>
2014-08-23 19:35:16 -0400

    typo in action mailer url

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -18,7 +18,7 @@ class UserMailer < ActionMailer::Base
     @recipient = User.find(rental.user_id)
     @group = Group.find(@sender.group_id)
     @tool = Tool.find(rental.tool_id)
-    @url  = "http://t00lshed.herokuapp.com/group/" + @group.id + "users/" + @recipient.id + "/tools/" + @tool.id + "/rentals/" + rental.id.to_s
+    @url  = "http://t00lshed.herokuapp.com/group/" + @group.id.to_s + "users/" + @recipient.id.to_s + "/tools/" + @tool.id.to_s + "/rentals/" + rental.id.to_s
     mail(to: @recipient.email, from: @sender.email, subject: 'Rental Request from' + @sender.name)
   end
 end
