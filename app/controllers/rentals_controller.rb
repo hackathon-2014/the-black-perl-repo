7c84a1a896436f8c70d6285fe9d7422ca084d688
maryhipp <maryhipp@gmail.com>
2014-08-23 17:43:03 -0400

    user mailer added to rental controller

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -2,11 +2,13 @@ class RentalsController < ApplicationController
 
   def new
     @rental = Rental.new
+
   end
 
   def create
     @rental = Rental.create rental_params
     if @rental.save
+      UserMailer.rental_request_email(@rental).deliver
       redirect_to root_path
     else
       render :new
