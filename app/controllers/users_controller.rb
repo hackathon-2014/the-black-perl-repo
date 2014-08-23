8acd626bd0fac494fea562f5eec854ce847ae169
Jamie Kingston <jamie.kingston@gmail.com>
2014-08-23 17:31:42 -0400

    added member links and rental views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,13 @@
 class UsersController < ApplicationController
 
+  before_action :find_user
+
   def index
     
   end
 
   def show
-    
+ 
   end
 
   def new
@@ -26,6 +28,9 @@ class UsersController < ApplicationController
 
   private
 
+    def find_user
+      @user = User.find params[:id]
+    end
   
 
 end
