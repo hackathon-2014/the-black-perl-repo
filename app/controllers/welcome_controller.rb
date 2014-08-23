b29ae065d1013b2a329a6143924c48abdc8808b7
maryhipp <maryhipp@gmail.com>
2014-08-23 13:35:14 -0400

    changed homepage to group show

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,9 @@
 class WelcomeController < ApplicationController
 
   # skip_before_action :authenticate_user!
-
+  def index
+    binding.pry
+    @group = Group.find(current_user.group_id)
+    @admin = User.find(@group.user_id)
+  end
 end
