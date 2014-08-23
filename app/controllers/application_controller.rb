0d4378cb7664d4eadd5a3eafcb65ca710fa902f2
Jamie Kingston <jamie.kingston@gmail.com>
2014-08-23 13:45:36 -0400

    added logout button and edit registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :authenticate_user!
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit({ roles: [] }, :email, :password, :password_confirmation, :first_name, :last_name, :role, :group_id) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit({ roles: [] }, :email, :password, :password_confirmation, :first_name, :last_name, :current_password, :role, :group_id) }
+  end
+
 end
