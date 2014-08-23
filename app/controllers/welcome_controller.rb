2bfcd2dc2bf6cee6d4c300cc9a7612a6bd18604b
maryhipp <maryhipp@gmail.com>
2014-08-23 14:51:50 -0400

    removed pry

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,7 +5,6 @@ class WelcomeController < ApplicationController
 
   # skip_before_action :authenticate_user!
   def index
-    binding.pry
     @group = Group.find(current_user.group_id)
     @admin = User.find(@group.user_id)
     @members = User.where(group_id: @group.id)
