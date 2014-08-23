76becd113591f996cad2c77e084827c9610537ab
maryhipp <maryhipp@gmail.com>
2014-08-23 13:50:21 -0400

    list group members on page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,5 +4,7 @@ class WelcomeController < ApplicationController
   def index
     @group = Group.find(current_user.group_id)
     @admin = User.find(@group.user_id)
+    @members = User.where(group_id: @group.id)
   end
 end
+
